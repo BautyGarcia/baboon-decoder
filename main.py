a7e9d551a5641fe88c67a9d65352f6cc701e064f
Bauty Garcia <bautistaroccogarcia@gmail.com>
2024-05-10 22:30:52 -0300

    feat(styling): enhanced showing section

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ st.sidebar.write("¿Nunca te pasó de tener una foto de un babuino, pero está e
 st.sidebar.write("¡No te preocupes! Con esta herramienta podrás desencriptarla (si está bien encriptada).")
 
 # Main area for the decoder
-st.header("Carga tu imagen")
-data = st.file_uploader("", type=['png', 'jpg', 'jpeg'])
+st.header("Pasame tu baboon", anchor=False)
+data = st.file_uploader("Subir archivo", label_visibility="hidden", type=['png', 'jpg', 'jpeg'])
 
 if data is not None:
     # Display the uploaded image
@@ -23,7 +23,7 @@ if data is not None:
             result = decodeImage(Image.open(data))
             # Display the result
             st.write("Resultado de la desencriptación:")
-            st.write(result if result else "No se encontró nada en la imagen.")
+            st.markdown(f"<p style='font-size: 30px; font-weight: bold;'>{result if result else "Lo intente, pero no pude encontrar nada"}</p>", unsafe_allow_html=True)
             
         except Exception as e:
             st.write("No che, algo no funciona bien, Asegúrate de que la imagen esté bien encriptada.")
