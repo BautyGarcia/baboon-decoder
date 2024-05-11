8fb1675cfbdfaefdd928ec46d5554f7353e8e7a7
Bauty Garcia <bautistaroccogarcia@gmail.com>
2024-05-10 22:43:00 -0300

    feat(styling): enhanced again the show result island styling

--- main.py
+++ main.py
@@ -17,13 +17,31 @@ if data is not None:
     
     # Button to decode the image
     if st.button('Desencriptar'):
-        # Call the decodeImage function with the file path
-        
         try:
             result = decodeImage(Image.open(data))
-            # Display the result
-            st.write("Resultado de la desencriptación:")
-            st.markdown(f"<p style='font-size: 30px; font-weight: bold;'>{result if result else "Lo intente, pero no pude encontrar nada"}</p>", unsafe_allow_html=True)
-            
+
+            # Use a container to create a styled section for displaying the result
+            with st.container():
+                st.markdown("""
+                    <style>
+                    .result-container {
+                        background-color: #262730;
+                        border-radius: 10px;
+                        padding: 20px;
+                        margin: 10px 0px;
+                    }
+                    .subres {
+                        font-size: 20px;
+                        font-weight: bolder;
+                        color: #f0f0f0;
+                    }
+                    </style>
+                    """, unsafe_allow_html=True)
+                
+                # Display the result inside the styled container
+                if result:
+                    st.markdown(f"<div class='result-container'><p class='subres'>Esto es lo que pude encontrar:</p><p style='font-size: 30px; font-weight: bold;'>{result}</p></div>", unsafe_allow_html=True)
+                else:
+                    st.error("Lo intenté, pero no pude encontrar nada.")
         except Exception as e:
-            st.write("No che, algo no funciona bien, Asegúrate de que la imagen esté bien encriptada.")
+            st.error("No che, algo no funciona bien, asegúrate de que la imagen esté bien encriptada.")
